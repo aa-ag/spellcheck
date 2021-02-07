dd427262d44f52219e6ad0a381f25b55fe1bcea4
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-07 14:59:45 -0600

    correcting spelling, there are limitations

--- script.py
+++ script.py
@@ -11,37 +11,20 @@ such as part-of-speech tagging, noun phrase extraction, sentiment analysis, clas
 # https://textblob.readthedocs.io/en/dev/
 
 ###--- GLOBAL VARIABLES ---###
-text = '''
-The titular threat of The Blob has always struck me as the ultimate movie
-monster: an insatiably hungry, amoeba-like mass able to penetrate
-virtually any safeguard, capable of--as a doomed doctor chillingly
-describes it--"assimilating flesh on contact.
-Snide comparisons to gelatin be damned, it's a concept with the most
-devastating of potential consequences, not unlike the grey goo scenario
-proposed by technological theorists fearful of
-artificial intelligence run rampant.
-'''
+txt = "I havv good spelling."
 
 
 ###--- FUNCTIONS ---###
-def sentiment_and_polarity():
-    blob = TextBlob(text)
-    blob.tags
-    # [('The', 'DT'), ('titular', 'JJ'), ('threat', 'NN'), ('of', 'IN'), ...]
-
-    blob.noun_phrases
-    # WordList(['titular threat', 'blob',
-    # 'ultimate movie monster', 'amoeba-like mass', ...])
-
-    for sentence in blob.sentences:
-        print(sentence.sentiment.polarity)
-
+def correct_spelling():
     '''
-    0.06000000000000001
-    -0.34166666666666673
+      Use the correct() method 
+      to attempt spelling correction.
     '''
+    global txt
+    usable = TextBlob(txt)
+    print(usable.correct())
 
 
 ###--- DRIVER CODE ---###
 if __name__ == "__main__":
-    sentiment_and_polarity()
+    correct_spelling()
